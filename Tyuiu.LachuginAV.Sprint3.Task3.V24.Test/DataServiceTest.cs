df26eb084715a039f338752623bb3205d59593e2
Tyuiu.LachuginAV@outlook.com <lavtemik@mail.ru>
2023-10-22 19:08:38 +0300

    Task3

---
+++ Tyuiu.LachuginAV.Sprint3.Task3.V24.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.LachuginAV.Sprint3.Task3.V24.Lib;
+
+namespace Tyuiu.LachuginAV.Sprint3.Task3.V24.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValueExpression()
+        {
+            DataService ds = new DataService();
+            string a = "****** ";
+            var res = ds.ReplaceCharInString(a, '*', 'c');
+            Assert.AreEqual(res, "cccccc ");
+
+
+        }
+    }
+}
