3ac9abe33d178eeefd39896c2d46c4e62e4157ff
Tyuiu.LachuginAV@outlook.com <lavtemik@mail.ru>
2023-10-28 19:45:24 +0300

    Task5

---
+++ Tyuiu.LachuginAV.Sprint3.Task5.V2.Test/DataServiceTest.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.LachuginAV.Sprint3.Task5.V2.Lib;
+
+namespace Tyuiu.LachuginAV.Sprint3.Task5.V2.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidGetSumSumSeries()
+        {
+            DataService ds = new DataService();
+            int x = 5;
+            int start1 = 1; int stop1 = 3;
+            int start2 = 1; int stop2 = 12;
+
+            Assert.AreEqual(35.658, ds.GetSumSumSeries(x, start1, start2, stop1, stop2));
+        }
+    }
+}
---
+++ Tyuiu.LachuginAV.Sprint3.Task5.V2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.LachuginAV.Sprint3.Task5.V2.Lib;
+
+namespace Tyuiu.LachuginAV.Sprint3.Task5.V2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #3 | Выполнил: Лаяцгин. А.В | АСОиУБ-23-3";
+            Console.WriteLine(String.Concat(Enumerable.Repeat("*", 75)));
+            Console.WriteLine("* Спринт #3" + String.Concat(Enumerable.Repeat(" ", 63)) + "*");
+            Console.WriteLine("* Тема: Вложенные циклы                                                   *");
+            Console.WriteLine("* Задание #5" + String.Concat(Enumerable.Repeat(" ", 62)) + "*");
+            Console.WriteLine("* Вариант 2" + String.Concat(Enumerable.Repeat(" ", 63)) + "*");
+            Console.WriteLine("* Выполнил: Лачугин Артем Викторович | АСОиУБ-23-3" + String.Concat(Enumerable.Repeat(" ", 26)) + "*");
+            Console.WriteLine(String.Concat(Enumerable.Repeat("*", 75)));
+            Console.WriteLine("* УСЛОВИЕ:" + String.Concat(Enumerable.Repeat(" ", 64)) + "*");
+            Console.WriteLine("* Написать програму, которая запрашивает у пользователя данные, вычисляет *");
+            Console.WriteLine("* их и печатает их на экран.                                              *");
+            Console.WriteLine("*" + String.Concat(Enumerable.Repeat(" ", 73)) + "*");
+            Console.WriteLine(String.Concat(Enumerable.Repeat("*", 75)));
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:" + String.Concat(Enumerable.Repeat(" ", 56)) + "*");
+            Console.WriteLine(String.Concat(Enumerable.Repeat("*", 75)));
+
+            int x = 5, startValue1 = 1, startValue2 = 1, stopValue1 = 3, stopValue2 = 12;
+
+
+            Console.WriteLine(String.Concat(Enumerable.Repeat("*", 75)));
+            Console.WriteLine("* Результат:                                                              *");
+            Console.WriteLine(String.Concat(Enumerable.Repeat("*", 75)));
+
+            Console.WriteLine($"Сумма двойного ряда равна:\n{ds.GetSumSumSeries( x, startValue1, startValue2, stopValue1, stopValue2)}");
+            Console.ReadKey();
+        }
+    }
+}
